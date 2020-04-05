209de45d134e8bb6d67a8f7591013dbe4cfc79cc
taha uygun <tahauygun@gmail.com>
2020-04-04 22:44:50 -0700

    looping object

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,28 @@
 <template>
   <div>
-<!--    <p v-if="show">You can see me!</p>-->
-<!--    <p v-else>Now you see me!</p>-->
-<!--    <template v-if="show">-->
-<!--      <h2>Heading</h2>-->
-<!--      <p>Inside a template</p>-->
-<!--    </template>-->
-<!--    <p v-show="show">Do you see me!</p>-->
-<!--    <button @click="show =!show">Switch</button>-->
-  <ul>
-    <li v-for="(ingredient, i) in ingredients" :key="ingredient">
-      {{ingredient}} ({{i}})
-    </li>
-  </ul>
+    <!--    <p v-if="show">You can see me!</p>-->
+    <!--    <p v-else>Now you see me!</p>-->
+    <!--    <template v-if="show">-->
+    <!--      <h2>Heading</h2>-->
+    <!--      <p>Inside a template</p>-->
+    <!--    </template>-->
+    <!--    <p v-show="show">Do you see me!</p>-->
+    <!--    <button @click="show =!show">Switch</button>-->
+    <ul>
+      <li v-for="(ingredient, i) in ingredients" :key="ingredient">
+        {{ ingredient }} ({{ i }})
+      </li>
+    </ul>
+    <hr />
+    <ul>
+      <li v-for="person in persons" :key="person">
+        <span v-for="value in person" :key="value"> {{value}} </span>
+      </li>
+    </ul>
+    <!--    <template v-for="(ingredient, index) in ingredients">-->
+    <!--      <h1>{{ingredient}}</h1>-->
+    <!--      <p>{{index}}</p>-->
+    <!--    </template>-->
   </div>
 </template>
 
@@ -22,10 +32,10 @@ export default {
   data: function() {
     return {
       // show: true
-      ingredients:['meat','fruit','cookies'],
+      ingredients: ["meat", "fruit", "cookies"],
       persons: [
-        {name: 'Max', age:27, color:'red'},
-        {name: 'Anna', age: 'unknown', color:'blue'}
+        { name: "Max", age: 27, color: "red" },
+        { name: "Anna", age: "unknown", color: "blue" }
       ]
     };
   },
