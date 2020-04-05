c42ed7bca4ef6db558d10deac757bf8b26c2b786
taha uygun <tahauygun@gmail.com>
2020-04-05 13:53:54 -0700

    adding other actions

--- src/components/MonsterAttack.vue
+++ src/components/MonsterAttack.vue
@@ -73,26 +73,45 @@ export default {
         return;
       }
 
+      this.monsterAttacks();
+    },
+    specialAttack: function() {
+      this.monsterHealth -= this.calculateDamage(10, 20);
+
+      if (this.checkWin()) {
+        return;
+      }
       this.playerHealth -= this.calculateDamage(5, 12);
 
       this.checkWin();
     },
-    specialAttack: function() {},
-    heal: function() {},
+    heal: function() {
+      if (this.playerHealth <= 90) {
+        this.playerHealth += 10;
+      } else {
+        this.playerHealth = 100;
+      }
+      this.monsterAttacks();
+    },
     giveUp: function() {},
-    calculateDamage: function(min, max){
+    monsterAttacks: function() {
+      this.playerHealth -= this.calculateDamage(5, 12);
+
+      this.checkWin();
+    },
+    calculateDamage: function(min, max) {
       return Math.max(Math.floor(Math.random * max) + 1, min);
     },
-    checkWin: function () {
+    checkWin: function() {
       if (this.monsterHealth <= 0) {
-        if (confirm('You won! New Game?')) {
+        if (confirm("You won! New Game?")) {
           this.startGame();
         } else {
           this.gameIsRunning = false;
         }
         return true;
       } else if (this.playerHealth <= 0) {
-        if (confirm('You lost! New Game?')) {
+        if (confirm("You lost! New Game?")) {
           this.startGame();
         } else {
           this.gameIsRunning = false;
