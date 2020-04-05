86e9abdf5dbc6e5e4ffaaebf07a77560c2896252
taha uygun <tahauygun@gmail.com>
2020-04-05 14:02:18 -0700

    creating an action log

--- src/components/MonsterAttack.vue
+++ src/components/MonsterAttack.vue
@@ -57,7 +57,8 @@ export default {
     return {
       playerHealth: 100,
       monsterHealth: 100,
-      gameIsRunning: false
+      gameIsRunning: false,
+      turns:[]
     };
   },
   methods: {
@@ -67,7 +68,12 @@ export default {
       this.monsterHealth = 100;
     },
     attack: function() {
-      this.monsterHealth -= this.calculateDamage(3, 10);
+      var damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer:true,
+        text: 'Player hits Monster for ' + damage
+      });
 
       if (this.checkWin()) {
         return;
@@ -93,11 +99,18 @@ export default {
       }
       this.monsterAttacks();
     },
-    giveUp: function() {},
+    giveUp: function() {
+      this.gameIsRunning = false;
+    },
     monsterAttacks: function() {
-      this.playerHealth -= this.calculateDamage(5, 12);
+      var damage =  this.calculateDamage(5, 12)
+      this.playerHealth -= damage;
 
       this.checkWin();
+      this.turns.unshift({
+        isPlayer:false,
+        text: 'Monster hits Player for ' + damage
+    });
     },
     calculateDamage: function(min, max) {
       return Math.max(Math.floor(Math.random * max) + 1, min);
