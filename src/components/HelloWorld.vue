e1ba2863d225ca7cd318b48481958e56f931c7c7
taha uygun <tahauygun@gmail.com>
2020-04-04 22:32:10 -0700

    v-for

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    <p v-if="show">You can see me!</p>
-    <p v-else>Now you see me!</p>
-    <template v-if="show">
-      <h2>Heading</h2>
-      <p>Inside a template</p>
-    </template>
-    <p v-show="show">Do you see me!</p>
-    <button @click="show =!show">Switch</button>
+<!--    <p v-if="show">You can see me!</p>-->
+<!--    <p v-else>Now you see me!</p>-->
+<!--    <template v-if="show">-->
+<!--      <h2>Heading</h2>-->
+<!--      <p>Inside a template</p>-->
+<!--    </template>-->
+<!--    <p v-show="show">Do you see me!</p>-->
+<!--    <button @click="show =!show">Switch</button>-->
+  <ul>
+    <li v-for="ingredient in ingredients" :key="ingredient">
+      {{ingredient}}
+    </li>
+  </ul>
   </div>
 </template>
 
@@ -16,7 +21,12 @@ export default {
   name: "HelloWorld",
   data: function() {
     return {
-      show: true
+      // show: true
+      ingredients:['meat','fruit','cookies'],
+      persons: [
+        {name: 'Max', age:27, color:'red'},
+        {name: 'Anna', age: 'unknown', color:'blue'}
+      ]
     };
   },
   props: {
