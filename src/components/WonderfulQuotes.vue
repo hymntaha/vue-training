fac1c026c184dbf8e34adfc9531382ab33cdab7a
taha uygun <tahauygun@gmail.com>
2020-04-06 23:38:57 -0700

    setting up the component

--- src/components/WonderfulQuotes.vue
+++ src/components/WonderfulQuotes.vue
@@ -1,9 +1,19 @@
 <template>
+    <div>
 
+    </div>
 </template>
 <script>
   export default{
-
+    name: "WonderfulQuotes",
+    data: function(){
+      return{
+        quotes: [
+          'Just a Quote to see something'
+        ],
+        maxQuotes: 10
+      }
+    }
   }
 </script>
 <style>
