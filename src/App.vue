8cd8a547512984fbb8bc7bb97c7648a4845f973b
taha uygun <tahauygun@gmail.com>
2020-04-06 23:37:10 -0700

    adding wonderful quotes project

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
     <MonsterAttack/>
+    <WonderfulQuotes/>
   </div>
 </template>
 
 <script>
 import MonsterAttack from './components/MonsterAttack'
-
+import WonderfulQuotes from "./components/WonderfulQuotes";
 export default {
   name: 'App',
   components: {
     MonsterAttack,
+    WonderfulQuotes
   }
 }
 </script>
