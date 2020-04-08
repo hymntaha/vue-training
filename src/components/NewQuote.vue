a4c81feb941200350a35cf5a9762e83fb9d6c07d
taha uygun <tahauygun@gmail.com>
2020-04-07 22:01:47 -0700

    setting up quote form

--- src/components/NewQuote.vue
+++ src/components/NewQuote.vue
@@ -4,5 +4,22 @@
             <label for="quote">Quote</label>
             <textarea class=form-control" v-model="quote" name="" id="" cols="30" rows="10"></textarea>
         </div>
+        <div class="col-md-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+            <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+        </div>
     </div>
 </template>
+<script>
+    export default {
+      data: function () {
+        return {
+          quote:''
+        }
+      },
+      methods:{
+        createNew(){
+
+        }
+      }
+    }
+</script>
