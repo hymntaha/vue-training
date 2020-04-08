38ad2d9e91985715c8512f0351eecda6a5508dcb
taha uygun <tahauygun@gmail.com>
2020-04-07 22:00:02 -0700

    setting up quote form

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="row">
+        <div class="col-md-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+            <label for="quote">Quote</label>
+            <textarea class=form-control" v-model="quote" name="" id="" cols="30" rows="10"></textarea>
+        </div>
+    </div>
+</template>
